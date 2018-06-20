8c538284bbcb31fc2a4478d5a0b186dc52e30566
William Lim <manvinedi@gmail.com>
2018-06-20 20:23:00 +0800

    partials example

---
+++ Partials/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Partials
+{
+    public partial class CoOrds
+    {
+        private int x;
+        private int y;
+
+        public CoOrds(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public partial class CoOrds
+    {
+        public void PrintCoOrds()
+        {
+            Console.WriteLine("CoOrds: {0},{1}", x, y);
+        }
+    }
+
+    class TestCoOrds
+    {
+        static void Main()
+        {
+            CoOrds myCoOrds = new CoOrds(10, 15);
+
+            // Output: CoOrds: 10,15
+            myCoOrds.PrintCoOrds();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
